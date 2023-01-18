49a06bb942becbfe9628371dcc4589915af59ee3
MrLux0r <007.korol@gmail.com>
2023-01-18 17:59:26 +0200

    Задание: запуск автотестов для разных языков интерфейса

---
+++ test_items.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_link(browser):
+    browser.get(link)
+    time.sleep(5)
+
+    css_selector = "form.add-to-basket .btn.btn-lg.btn-primary.btn-add-to-basket"
+    def test_check_by_css_selector(css_selector):
+        try:
+            browser.find_element(By.CSS_SELECTOR, css_selector)
+        except NoSuchElementException:
+            return False
+        return True
+    assert test_check_by_css_selector(css_selector) == True, "корзинка не найдена"
+
